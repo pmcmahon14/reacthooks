5c35f93dd0e5ae563b7db5f81a583dc29a4307b3
Patrick McMahon <pmcmahon14@gmail.com>
2019-07-14 20:34:10 -0700

    added useEffect hook

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,24 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Todo = props => {
     const [todoName, setTodoName] = useState('');
     const [todoList, setTodoList] = useState([]);
+    //   const [todoState, setTodoState] = useState({ userInput: '', todoList: [] });
 
+    useEffect(() => {
+        axios.get('https://test-3e15a.firebaseio.com/todos.json').then(result => {
+            console.log(result);
+            const todoData = result.data;
+            const todos = [];
+            for (const key in todoData) {
+                todos.push({id: key, name: todoData[key].name})
+            }
+            setTodoList(todos);
+        });
+    });
     const inputChangeHandler = event => {
+        // setTodoState({
+        //   userInput: event.target.value,
+        //   todoList: todoState.todoList
+        // });
         setTodoName(event.target.value);
     };
 
     const todoAddHandler = () => {
+        // setTodoState({
+        //   userInput: todoState.userInput,
+        //   todoList: todoState.todoList.concat(todoState.userInput)
+        // });
         setTodoList(todoList.concat(todoName));
         axios
-            .post('', {name: todoName})
+            .post('https://test-3e15a.firebaseio.com/todos.json', { name: todoName })
             .then(res => {
                 console.log(res);
             })
             .catch(err => {
                 console.log(err);
-        });
+            });
     };
 
     return (
@@ -34,7 +54,7 @@ const Todo = props => {
             </button>
             <ul>
                 {todoList.map(todo => (
-                    <li key={todo}>{todo}</li>
+                    <li key={todo.id}>{todo.name}</li>
                 ))}
             </ul>
         </React.Fragment>
